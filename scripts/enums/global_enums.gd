d2b39232080c1873a5f4b05f00c113536360b950
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-10 15:26:26 -0400

    Formatted and added Mood enums

--- scripts/enums/global_enums.gd
+++ scripts/enums/global_enums.gd
@@ -1,18 +1,12 @@
-extends RefCounted
+## Global enums for the entire game.
 class_name GlobalEnums
+extends RefCounted
 
 ## Represents pet (or entity)'s gender. If set to NONE, they will be genderless.
-enum Gender {
-	NONE,
-	MALE,
-	FEMALE
-}
+enum Gender { NONE, MALE, FEMALE }
 
-## Represents a flavor. Foods can have more than one flavor assigned.
-enum Flavor {
-	SPICY,
-	DRY,
-	SWEET,
-	BITTER,
-	SOUR
-}
+## Represents a flavor. Anything that uses flavors can have more than one flavor assigned.
+enum Flavor { SPICY, DRY, SWEET, BITTER, SOUR }
+
+## Represents a pet's mood.
+enum Mood { NEUTRAL, HAPPY, HUNGRY, BORED, TIRED, UPSET }
