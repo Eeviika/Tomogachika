f67770b8f0e9c7ba94475431872ff17136db489f
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-12 17:28:54 -0400

    Made create_timestamp func and changed TimeHelper funcs to use timestamps

--- scripts/classes/time_helper.gd
+++ scripts/classes/time_helper.gd
@@ -2,13 +2,13 @@ class_name TimeHelper
 extends RefCounted
 
 
-static func _is_valid_time_dict(time: Dictionary[String, int]) -> bool:
-	if not ("hour" in time and "minute" in time and "second" in time):
+static func _is_valid_time_dict(time_dict: Dictionary[String, int]) -> bool:
+	if not ("hour" in time_dict and "minute" in time_dict and "second" in time_dict):
 		return false
 
-	var hour: int = time.get("hour", -1)
-	var minute: int = time.get("minute", -1)
-	var second: int = time.get("second", -1)
+	var hour: int = time_dict.get("hour", -1)
+	var minute: int = time_dict.get("minute", -1)
+	var second: int = time_dict.get("second", -1)
 
 	if hour < 0 or hour > 23:
 		return false
@@ -20,12 +20,17 @@ static func _is_valid_time_dict(time: Dictionary[String, int]) -> bool:
 	return true
 
 
-static func is_past_time(
-	current_time: Dictionary[String, int], target_time: Dictionary[String, int]
-) -> bool:
-	if not (_is_valid_time_dict(current_time) and _is_valid_time_dict(target_time)):
-		return false
+static func create_timestamp(time_dict: Dictionary[String, int]) -> Timestamp:
+	assert(_is_valid_time_dict(time_dict), "Cannot create a timestamp from an invalid time_dict.")
+	var new_timestamp: Timestamp = Timestamp.new()
+	new_timestamp.hour = time_dict.get("hour")
+	new_timestamp.minute = time_dict.get("minute")
+	new_timestamp.second = time_dict.get("second")
 
+	return new_timestamp
+
+
+static func is_past_time(current_time: Timestamp, target_time: Timestamp) -> bool:
 	if current_time.hour != target_time.hour:
 		return current_time.hour > target_time.hour
 	if current_time.minute != target_time.minute:
@@ -33,13 +38,7 @@ static func is_past_time(
 	return current_time.second > target_time.second
 
 
-static func is_before_time(
-	current_time: Dictionary[String, int], target_time: Dictionary[String, int]
-) -> bool:
-	if not (_is_valid_time_dict(current_time) and _is_valid_time_dict(target_time)):
-		return false
-
+static func is_before_time(current_time: Timestamp, target_time: Timestamp) -> bool:
 	if current_time == target_time:
 		return false
-
 	return not is_past_time(current_time, target_time)
