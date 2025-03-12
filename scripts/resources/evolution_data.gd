71a06ea95e18fb27c2cb1adbac1716d3a7d5321f
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-11 20:21:53 -0400

    Added EvolutionData resource variables

--- scripts/resources/evolution_data.gd
+++ scripts/resources/evolution_data.gd
@@ -1,1 +1,14 @@
+## Represents a possible evolution that a species may have.
+class_name EvolutionData
 extends Resource
+
+@export var evolution: SpeciesData
+
+@export_category("Conditions")
+@export var in_spring: bool = false
+@export var in_summer: bool = false
+@export var in_fall: bool = false
+@export var in_winter: bool = false
+@export var at_day: bool = false
+@export var at_night: bool = false
+@export var required_happiness: float = 0.0
