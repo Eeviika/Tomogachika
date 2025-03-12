350f2438aca68618b0b1420c102c804b2c710d6c
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-11 20:44:01 -0400

    Added documentation comments to Timestamp (would we really need this?)

--- scripts/resources/timestamp.gd
+++ scripts/resources/timestamp.gd
@@ -2,16 +2,22 @@
 class_name Timestamp
 extends Resource
 
+## The hour of this timestamp (in 24-hour time).
+## Cannot be below 0 or exceed 23.
 @export var hour: int = 0:
 	get:
 		return hour
 	set(value):
 		hour = floor(clamp(value, 0, 23))
+## The minute of this timestamp.
+## Cannot be below 0 or exceed 59.
 @export var minute: int = 0:
 	get:
 		return minute
 	set(value):
 		minute = floor(clamp(value, 0, 59))
+## The second of this timestamp.
+## Cannot be below 0 or exceed 59.
 @export var second: int = 0:
 	get:
 		return second
