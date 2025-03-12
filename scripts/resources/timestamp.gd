672a0d18609d6c9f10c779bb2b7e2922e326e02f
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-11 20:08:44 -0400

    Added timestamp hour, minute, second containers

--- scripts/resources/timestamp.gd
+++ scripts/resources/timestamp.gd
@@ -1,1 +1,19 @@
+## Represents a point in time.
+class_name Timestamp
 extends Resource
+
+@export var hour: int = 0:
+	get:
+		return hour
+	set(value):
+		hour = floor(clamp(value, 0, 23))
+@export var minute: int = 0:
+	get:
+		return minute
+	set(value):
+		minute = floor(clamp(value, 0, 59))
+@export var second: int = 0:
+	get:
+		return second
+	set(value):
+		second = floor(clamp(value, 0, 59))
