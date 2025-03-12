b7ae51bbba862affafcb80add4cbd7c5f27084a9
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-11 20:18:20 -0400

    Renamed PetInfo into SpeciesData for clarification, and added more variables to SpeciesData

---
+++ scripts/resources/species_data.gd
@@ -0,0 +1,29 @@
+## Represents the biology and statistics of a pet species.
+## Typically converted from a JSON file when packing mods.
+class_name PetInfo
+extends Resource
+
+@export_category("Common")
+@export var name: String = "Pet"
+@export var can_be_naturally_obtained: bool = false
+@export var scale: float = 1.00
+@export var collision_radius: float = 1.00
+@export var collision_offset: Vector2 = Vector2.ZERO
+
+@export_category("Gender")
+@export var can_be_female: bool = false
+@export var can_be_male: bool = false
+@export_range(-10.0, 10.0, 1) var gender_ratio: float = 0.0
+
+@export_category("Biology")
+@export var average_weight: float = 1.0
+@export var average_height: float = 1.0
+@export var height_mutation: float = 0.0
+@export var weight_mutation: float = 0.0
+@export var weight_is_influenced: bool = false
+@export var height_is_influenced: bool = false
+
+@export_category("Preferences")
+@export var flavor_preference: Array[GlobalEnums.Flavor]
+@export var bedtime: Timestamp
+@export var waketime: Timestamp
