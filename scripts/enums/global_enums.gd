e0665dbb933e7cec3b3077cf6c96a5649dbf46d5
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-03-14 14:07:16 -0400

    Added "ASLEEP" to Mood enums

--- scripts/enums/global_enums.gd
+++ scripts/enums/global_enums.gd
@@ -9,4 +9,4 @@ enum Gender { NONE, MALE, FEMALE }
 enum Flavor { SPICY, DRY, SWEET, BITTER, SOUR }
 
 ## Represents a pet's mood.
-enum Mood { NEUTRAL, HAPPY, HUNGRY, BORED, TIRED, UPSET }
+enum Mood { NEUTRAL, HAPPY, HUNGRY, BORED, TIRED, ASLEEP, UPSET }
