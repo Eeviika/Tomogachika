6c79014d51fc9185bd4f864e26f05dd91454121c
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-05-15 15:23:29 -0400

    Formatting + set animation names to StringNames

--- scripts/helpers/predefined_animation_rules.gd
+++ scripts/helpers/predefined_animation_rules.gd
@@ -2,61 +2,76 @@
 class_name PredefinedAnimationRules
 extends RefCounted
 
+
 static func idle_neutral() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "idle_neutral"
+	rule.animation_name = &"idle_neutral"
 	rule.priority = 0
 	rule.condition = func(actor):
 		return actor.velocity.x == 0 and actor.is_on_floor()
+
 	return rule
+
 
 static func move_right() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "move_right"
+	rule.animation_name = &"move_right"
 	rule.priority = 1
 	rule.condition = func(actor):
 		return actor.velocity.x > 0 and actor.is_on_floor()
+
 	return rule
+
 
 static func move_left() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "move_left"
+	rule.animation_name = &"move_left"
 	rule.priority = 1
 	rule.condition = func(actor):
 		return actor.velocity.x < 0 and actor.is_on_floor()
+
 	return rule
+
 
 static func jump_right() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "jump_right"
+	rule.animation_name = &"jump_right"
 	rule.priority = 2
 	rule.condition = func(actor):
 		return actor.velocity.y < 0 and actor.velocity.x > 0 and not actor.is_on_floor()
+
 	return rule
+
 
 static func jump_left() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "jump_left"
+	rule.animation_name = &"jump_left"
 	rule.priority = 2
 	rule.condition = func(actor):
 		return actor.velocity.y < 0 and actor.velocity.x < 0 and not actor.is_on_floor()
+
 	return rule
+
 
 static func fall_right() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "fall_right"
+	rule.animation_name = &"fall_right"
 	rule.priority = 2
 	rule.condition = func(actor):
 		return actor.velocity.y > 0 and actor.velocity.x > 0 and not actor.is_on_floor()
+
 	return rule
+
 
 static func fall_left() -> AnimationRule:
 	var rule = AnimationRule.new()
-	rule.animation_name = "fall_left"
+	rule.animation_name = &"fall_left"
 	rule.priority = 2
 	rule.condition = func(actor):
 		return actor.velocity.y > 0 and actor.velocity.x < 0 and not actor.is_on_floor()
+
 	return rule
+
 
 static func get_all() -> Array[AnimationRule]:
 	return [
