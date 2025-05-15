0e0414e1c5c12987350ac29a902f5ee3fa279f8d
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-05-15 15:23:42 -0400

    Formatting and added FoodQuality

--- scripts/helpers/global_enums.gd
+++ scripts/helpers/global_enums.gd
@@ -5,10 +5,12 @@ extends RefCounted
 ## Represents pet (or entity)'s gender. If set to NONE, they will be genderless.
 enum Gender { NONE, MALE, FEMALE }
 
-## Represents a flavor. Anything that uses flavors can have more than one flavor assigned.
+## Represents a flavor. Anything that uses flavors (other than food) can have more than one flavor assigned.
 enum Flavor { SPICY, DRY, SWEET, BITTER, SOUR }
 
 ## Represents a pet's mood.
-enum Mood { NEUTRAL, HAPPY, HUNGRY, BORED, TIRED, ASLEEP, UPSET, DISAPPOINTED }
+enum Mood {NEUTRAL, HAPPY, HUNGRY, BORED, TIRED, ASLEEP, UPSET, DISAPPOINTED}
 
-enum CheatCause { TIME_TRAVEL }
+enum FoodQuality {POOR, AVERAGE, TASTY, DELICIOUS, GOURMET}
+
+enum CheatCause {TIME_TRAVEL}
